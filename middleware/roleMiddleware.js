a422a6aaed6d62c3369cf12781630bf8a5a1e0ff
Pawan Kumar <kprem5607@gmail.com>
2025-04-29 00:32:06 +0530

    remove file

--- middleware/roleMiddleware.js
+++
@@ -1,9 +0,0 @@
-export const roleMiddleware = (roles) => {
-    return (req, res, next) => {
-      if (!roles.includes(req.user.role)) {
-        return res.status(403).json({ message: 'Forbidden' });
-      }
-      next();
-    };
-  };
-  
--- model/Token.js
+++
@@ -1,8 +0,0 @@
-import { DataTypes } from 'sequelize';
-import sequelize from '../config/db.js';
-
-export const Token = sequelize.define('Token', {
-  token: DataTypes.STRING,
-  userId: DataTypes.INTEGER,
-  expiresAt: DataTypes.DATE,
-});
